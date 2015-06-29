47dec2ace2dc444afb585ff82755cec5d51335ca
Victor Lellis <vmlellis@gmail.com>
2015-06-29 01:33:02 -0300

    removed no solution files

--- ruby_17/bonus.rb
+++
@@ -1,9 +0,0 @@
-
-numbers = [24, 13, 8, 65]
-p numbers.any? { |n| n > 20 }
-p numbers.select { |n| n > 20 }.first
-p numbers.select { |n| n > 20 }.last
-p numbers.select { |n| n <= 20 }.first
-p numbers.select { |n| n <= 20 }.last
-p numbers.map { |n| n * 2 }
-p numbers
